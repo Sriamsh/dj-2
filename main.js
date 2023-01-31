eb629eca920dbbc1fb13921b84c518d50aff9102
Sriamsh <100685282+Sriamsh@users.noreply.github.com>
2023-01-31 19:40:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ leftwristx=0;
 leftwristy=0;
 rightwristx=0;
 rightwristy=0;
+scoreLeftWrist=0;
 
 function preload()
 {
@@ -13,7 +14,6 @@ function setup()
 {
     canvas = createCanvas(600, 500);
     canvas.center();
-
 
     video=createCapture(VIDEO);
     video.hide();
@@ -27,9 +27,21 @@ function modelLoaded()
     console.log('PoseNet Is Working');
 }
 
-function draw()
-{
-    image(video,0,0,600,500);
+function draw() {
+	image(video, 0, 0, 600, 500);
+ 
+	fill("#FF0000");
+	stroke("#FF0000");
+ 
+	if(scoreLeftWrist > 0.2)
+	{
+		circle(leftWristX,leftWristY,20);
+		InNumberleftWristY = Number(leftWristY); 
+		remove_decimals = floor(InNumberleftWristY);
+		volume = remove_decimals/500;
+		document.getElementById("volume").innerHTML = "Volume = " + volume;		
+		song.setVolume(volume);	
+	}
 }
 
 function play()
@@ -44,6 +56,8 @@ function gotPoses(results)
 if(results.length>0)
 {
     console.log(results);
+    scoreLeftWrist=results[0].pose.keypoints[9].score;
+    console.log("Score of left wrist is"+scoreLeftWrist);
     leftwristx=results[0].pose.leftWrist.x;
     leftwristy=results[0].pose.leftWrist.y;
     console.log("Left WristX = "+leftwristx+"Left WristY = "+leftwristy);
